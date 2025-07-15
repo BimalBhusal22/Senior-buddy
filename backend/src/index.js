8487b8962bfb1f45debc30cd6f828670d70cda3d
BimalBhusal22 <bimalbhusal1st2075@gmail.com>
2025-07-15 17:36:44 +0545

    Added utils: asyncHandler, ApiError, ApiResponse

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,4 +5,17 @@ dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log(`Bimal, Express Server Running Error: ${error}`);
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(
+        `Bimal, Express Server is Running at port: ${process.env.PORT || 8000}`
+      );
+    });
+  })
+  .catch((error) => {
+    console.log("[2nd] Bimal, MongoDB Connection Error:", error);
+  });
