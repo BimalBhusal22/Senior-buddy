9f1f15cb6df7acce1a9ca90febd190de3c55f482
Chiranjivi Basnet <basnetchiran1010@gmail.com>
2025-07-22 12:54:41 +0545

    fetch mentors to ViewMentors

--- frontend/src/components/dashboard/ViewMentors.jsx
+++ frontend/src/components/dashboard/ViewMentors.jsx
@@ -1,79 +1,93 @@
-// import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const ViewMentors = () => {
-  const mentors = [
-    {
-      mentorImage: "images/mentors/senior1.png",
-      mentorName: "Jivan",
-      mentorGender: "M",
-      mentorFaculty: "BSc. CSIT",
-      mentorPhoneNo: "9111111111",
-      mentorFbProfileLink: "https://www.facebook.com/jivan.gaire.79",
-      mentorEmail: "jivangaire@gmail.com",
-      collegeImage: "images/colleges/ButwalMultiple.jpg",
-      collegeName: "Butwal Multiple Campus",
-      collegeDistrict: "Rupandehi",
-      collegeLevels: ["bachelor", "master"],
-      collegeFaculties: ["BSc CSIT", "BBA", "BSc", "BBS", "more"],
-      collegeWebsiteLink: "https://bumc.tu.edu.np/",
-    },
-    {
-      mentorImage: "images/mentors/senior1.png",
-      mentorName: "Jivan",
-      mentorGender: "M",
-      mentorFaculty: "BSc. CSIT",
-      mentorPhoneNo: "9111111111",
-      mentorFbProfileLink: "https://www.facebook.com/jivan.gaire.79",
-      mentorEmail: "jivangaire@gmail.com",
-      collegeImage: "images/colleges/ButwalMultiple.jpg",
-      collegeName: "Butwal Multiple Campus",
-      collegeDistrict: "Rupandehi",
-      collegeLevels: ["bachelor", "master"],
-      collegeFaculties: ["BSc CSIT", "BBA", "BSc", "BBS", "more"],
-      collegeWebsiteLink: "https://bumc.tu.edu.np/",
-    },
-    {
-      mentorImage:
-        "https://upload.wikimedia.org/wikipedia/commons/e/ef/Virat_Kohli_during_the_India_vs_Aus_4th_Test_match_at_Narendra_Modi_Stadium_on_09_March_2023.jpg",
-      mentorName: "Jivan",
-      mentorGender: "M",
-      mentorFaculty: "BSc. CSIT",
-      mentorPhoneNo: "9111111111",
-      mentorFbProfileLink: "https://www.facebook.com/jivan.gaire.79",
-      mentorEmail: "jivangaire@gmail.com",
-      collegeImage: "images/colleges/ButwalMultiple.jpg",
-      collegeName: "Butwal Multiple Campus",
-      collegeDistrict: "Rupandehi",
-      collegeLevels: ["bachelor", "master"],
-      collegeFaculties: ["BSc CSIT", "BBA", "BSc", "BBS", "more"],
-      collegeWebsiteLink: "https://bumc.tu.edu.np/",
-    },
-  ];
+  // const mentors = [
+  //   {
+  //     mentorImage: "images/mentors/senior1.png",
+  //     mentorName: "Jivan",
+  //     mentorGender: "M",
+  //     mentorFaculty: "BSc. CSIT",
+  //     mentorPhoneNo: "9111111111",
+  //     mentorFbProfileLink: "https://www.facebook.com/jivan.gaire.79",
+  //     mentorEmail: "jivangaire@gmail.com",
+  //     collegeImage: "images/colleges/ButwalMultiple.jpg",
+  //     collegeName: "Butwal Multiple Campus",
+  //     collegeDistrict: "Rupandehi",
+  //     collegeLevels: ["bachelor", "master"],
+  //     collegeFaculties: ["BSc CSIT", "BBA", "BSc", "BBS", "more"],
+  //     collegeWebsiteLink: "https://bumc.tu.edu.np/",
+  //   },
+  //   {
+  //     mentorImage: "images/mentors/senior1.png",
+  //     mentorName: "Jivan",
+  //     mentorGender: "M",
+  //     mentorFaculty: "BSc. CSIT",
+  //     mentorPhoneNo: "9111111111",
+  //     mentorFbProfileLink: "https://www.facebook.com/jivan.gaire.79",
+  //     mentorEmail: "jivangaire@gmail.com",
+  //     collegeImage: "images/colleges/ButwalMultiple.jpg",
+  //     collegeName: "Butwal Multiple Campus",
+  //     collegeDistrict: "Rupandehi",
+  //     collegeLevels: ["bachelor", "master"],
+  //     collegeFaculties: ["BSc CSIT", "BBA", "BSc", "BBS", "more"],
+  //     collegeWebsiteLink: "https://bumc.tu.edu.np/",
+  //   },
+  //   {
+  //     mentorImage:
+  //       "https://upload.wikimedia.org/wikipedia/commons/e/ef/Virat_Kohli_during_the_India_vs_Aus_4th_Test_match_at_Narendra_Modi_Stadium_on_09_March_2023.jpg",
+  //     mentorName: "Jivan",
+  //     mentorGender: "M",
+  //     mentorFaculty: "BSc. CSIT",
+  //     mentorPhoneNo: "9111111111",
+  //     mentorFbProfileLink: "https://www.facebook.com/jivan.gaire.79",
+  //     mentorEmail: "jivangaire@gmail.com",
+  //     collegeImage: "images/colleges/ButwalMultiple.jpg",
+  //     collegeName: "Butwal Multiple Campus",
+  //     collegeDistrict: "Rupandehi",
+  //     collegeLevels: ["bachelor", "master"],
+  //     collegeFaculties: ["BSc CSIT", "BBA", "BSc", "BBS", "more"],
+  //     collegeWebsiteLink: "https://bumc.tu.edu.np/",
+  //   },
+  // ];
 
-  //   const [mentors, setMentors] = useState([]);
+  const [mentors, setMentors] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  //   useEffect(() => {
-  //     const fetchData = async () => {
-  //       try {
-  //         // Replace with actual API call
-  //         const response = await fetch("/api/mentors");
-  //         const data = await response.json();
-  //         setMentors(data);
-  //       } catch (error) {
-  //         console.error("Error fetching mentors:", error);
-  //       }
-  //     };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Replace with actual API call
+        const response = await fetch(
+          "http://localhost:7000/api/v1/admin/get_all_mentors"
+        );
+        if (!response.ok) {
+          throw new Error("Failed to fetch users");
+        }
+        const data = await response.json();
+        console.log(data.data);
+        setMentors(data.data);
+      } catch (error) {
+        console.error("Error fetching mentors:", error);
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-  //     fetchData();
-  //   }, []);
+    fetchData();
+  }, []);
 
-  if (mentors.length === 0) {
-    return (
-      <div className="text-center mt-5 text-muted">
-        Loading mentor details...
-      </div>
-    );
-  }
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+
+  // if (mentors.length === 0) {
+  //   return (
+  //     <div className="text-center mt-5 text-muted">
+  //       Loading mentor details...
+  //     </div>
+  //   );
+  // }
 
   return (
     <div className="container my-5">
